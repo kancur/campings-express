a028945f3df2195dacc946a861bfad89a80d2ee5
Peter Smid <petosmid@gmail.com>
2021-10-12 23:23:42 +0200

    initial commit

---
+++ models/village.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var VillageSchema = new Schema(
+  {},
+  {
+    strict:false,
+    collection : 'villages'
+  }
+);
+
+//Export model
+module.exports = mongoose.model('Village', VillageSchema);
