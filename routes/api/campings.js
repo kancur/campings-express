9c472890c368713ffa9cf5dd5bb2c7128bbfb3c0
Peter Smid <petosmid@gmail.com>
2021-10-13 23:03:30 +0200

    new camping api route for detail view

--- routes/api/campings.js
+++ routes/api/campings.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 const campingControler = require('../../controllers/campingController')
 
-//router.get('/', campingController.camping_get);
+router.get('/:id',  campingControler.camping_detail_get);
+router.get('/',  campingControler.camping_get);
 
 router.post('/', campingControler.camping_post)
 
