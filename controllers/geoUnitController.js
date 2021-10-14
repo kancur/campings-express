519dada396fd7a80c43169050e9d343e4f2f8fc2
Peter Smid <petosmid@gmail.com>
2021-10-14 10:03:06 +0200

    refractored to async/await

--- controllers/geoUnitController.js
+++ controllers/geoUnitController.js
@@ -1,38 +1,35 @@
 const GeoUnit = require("../models/geoUnit");
 
-exports.geoUnit_list = function (req, res, next) {
-  GeoUnit.find()
-    .select('uid')
-    .exec(function (err, geoMorphoUnits) {
-    if (err) {
-      return next(err);
-    }
-    res.json({ geo_units: geoMorphoUnits});
-  });
+exports.geoUnit_list = async function (req, res, next) {
+  try {
+    const data = await GeoUnit.find().select("_id properties.name properties.natural").exec();
+    res.json(data);
+  } catch (error) {
+    next(error);
+  }
 };
 
-exports.geoUnit_detail = function (req, res, next) {
+exports.geoUnit_detail = async function (req, res, next) {
   if (!req.query.id) {
-    next(new Error("Missing query id"));
+    next(new Error("Missing id parameter"));
     return;
   }
 
-  const hasGeoData = (req.query.geo == 'true') ? true : false;
+  const hasGeoData = req.query.geo == "true" ? true : false;
 
-  GeoUnit.findOne({ "properties.@id": req.query.id }).exec(function (
-    err,
-    geodata
-  ) {
-    if (err) {
-      return next(err);
-    }
-
+  try {
+    const data = GeoUnit.findOne({ "properties.@id": req.query.id }).lean().exec()
     if (hasGeoData) {
       res.json(geodata);
-      return
     }
-
-    const { geometry, ...exceptGeometry } = geodata.toObject();
+    // filtering out geometry out of data
+    const { geometry, ...exceptGeometry } = geodata;
     res.json(exceptGeometry);
-  });
+
+  } catch (error) {
+    next(error)   
+  }
+ 
+    
+  
 };
