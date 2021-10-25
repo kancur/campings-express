8c6b133691f9c89d5ed1e3cab07009131b9940bc
Peter Smid <petosmid@gmail.com>
2021-10-25 18:20:32 +0200

    add admin routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -6,12 +6,14 @@ const campingRoutes = require("./campingRoutes");
 const villageRoutes = require("./villageRoutes");
 const combinedRoutes = require("./combinedRoutes");
 const searchRoutes = require("./searchRoutes");
+const adminRoutes = require("./adminRoutes");
 
 router.use("/geo/", geoUnitRoutes);
 router.use("/camping/", campingRoutes);
 router.use("/village/", villageRoutes);
 router.use("/combined/", combinedRoutes);
 router.use("/search/", searchRoutes);
+router.use("/admin/", adminRoutes);
 
 router.get("/", function (req, res, next) {
   res.json({ title: "V1 API" });
