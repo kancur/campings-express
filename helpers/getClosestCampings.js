b08258e9ad11493faf5f396593efa1b7a519219c
Peter Smid <petosmid@gmail.com>
2021-10-31 17:15:35 +0100

    refractor and speed improvements

--- helpers/getClosestCampings.js
+++ helpers/getClosestCampings.js
@@ -17,14 +17,19 @@ async function getAllCampingsFromDB() {
  * @param {Number} coords.lon - Longitude in xx.xxxxxxx format
  * @returns
  */
-async function getCloseCampings(coords, limit = 10, maxDistance) {
-  const allCampings = await getAllCampingsFromDB();
 
-  // closestPoints function needs lat lon coordinates to be at the top of object (not nested)
-  const formatted = allCampings.map((camp) => ({ ...camp.coords, data: camp }));
+
+// closestPoints function needs lat lon coordinates to be at the top of object (not nested)
+let allCampings;
+let formatted;
+
+async function getCloseCampings(coords, limit = 10, maxDistance) {
+  if (!allCampings) {
+    allCampings = await getAllCampingsFromDB();
+    formatted = allCampings.map((camp) => ({ ...camp.coords, data: camp }));
+  }
   const closest = closestPoints(coords, formatted, true, limit, maxDistance);
   // extracting the original data
-  console.log("closest ------>",closest)
   if (closest) {
     return closest.map(({data, distance}) => ({...data, distance}))
   } else {
