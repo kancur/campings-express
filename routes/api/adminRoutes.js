8aec55b465c34458b3bb95f7368d1ae8c8e49dcf
Peter Smid <petosmid@gmail.com>
2021-10-31 17:15:48 +0100

    add route for village camp relation generation

--- routes/api/adminRoutes.js
+++ routes/api/adminRoutes.js
@@ -4,4 +4,5 @@ const adminController = require('../../controllers/adminController')
 
 router.get('/worker/camps-calculate-closest-villages', adminController.camping_calculate_closest_villages);
 router.get('/worker/camps-calculate-slugs', adminController.camping_calculate_slugs);
+router.get('/worker/villages-calculate-close-campings', adminController.villages_calculate_closest_campings)
 module.exports = router;
