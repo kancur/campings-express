dab9619ab741d887bd927f990b4d6f8277f945e6
Peter Smid <petosmid@gmail.com>
2021-10-31 13:55:35 +0100

    add worker for calculating slugs

--- routes/api/adminRoutes.js
+++ routes/api/adminRoutes.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const adminController = require('../../controllers/adminController')
 
 router.get('/worker/camps-calculate-closest-villages', adminController.camping_calculate_closest_villages);
-
+router.get('/worker/camps-calculate-slugs', adminController.camping_calculate_slugs);
 module.exports = router;
