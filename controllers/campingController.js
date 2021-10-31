5f44d02cb7236794391c9d98f611f14ff197263a
Peter Smid <petosmid@gmail.com>
2021-10-31 13:54:09 +0100

    add multipart-form handling

--- controllers/campingController.js
+++ controllers/campingController.js
@@ -1,4 +1,3 @@
-const testLib = require("../helpers/getClosestVillages");
 const { body, validationResult } = require("express-validator");
 const validationResultHandlerMiddleware = require("../middleware/validationResultHanderMiddleware");
 const closestVillagesMiddleware = require("../middleware/closestVillagesMiddleware");
@@ -6,6 +5,21 @@ const closeGeoUnitsMiddleware = require("../middleware/closeGeoUnitsMiddleware")
 const Camping = require("../models/camping");
 var ObjectId = require("mongoose").Types.ObjectId;
 const getClosestCampings = require("../helpers/getClosestCampings");
+const bodyLogger = require("../middleware/bodyLogger");
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/uploads/");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now();
+    cb(null, req.body.slug + "-" + "featured" + path.extname(file.originalname)); //Appending extension
+  },
+});
+
+const upload = multer({ storage: storage });
 
 exports.camping_detail_get = async function (req, res, next) {
   const id = req.params.id;
@@ -56,9 +70,18 @@ exports.camping_get = async function (req, res, next) {
 };
 
 exports.camping_create_post = [
+  upload.single("featured_image"),
+
+  async (req, res, next) => {
+    const payload = JSON.parse(req.body.payload);
+    req.body = { ...req.body, ...payload };
+    next();
+  },
+
+  bodyLogger,
   body("name", "name must be longer than 3 chars (camp name)").trim().isLength({ min: 3 }).escape(),
-  body("lat", "lat must be a numeric value (latitude)").trim().isNumeric().escape(),
-  body("lon", "lon must be a numeric value (longitude)").trim().isNumeric().escape(),
+  body("coords", "coords has to be object with lat and lon fields").isObject(),
+  body("slug", "slug is missing").trim().isString().isLength({ min: 3 }).escape(),
 
   //custom middleware
   validationResultHandlerMiddleware,
@@ -68,25 +91,50 @@ exports.camping_create_post = [
   //closeGeoUnitsMiddleware,
 
   async (req, res, next) => {
-    const geoUnits = res.locals.closeGeoUnits.map((geounit) => geounit._id);
+    //const geoUnits = res.locals.closeGeoUnits.map((geounit) => geounit._id);
+    //console.log(res.locals.closestVillages[0]);
 
-    console.log(res.locals.closestVillages[0]);
+    const filter = {};
 
-    const camp = new Camping({
+    if ( req.body._id ) {
+      filter["_id"] = req.body._id 
+    } else  {
+      filter["_id"] = ObjectId()
+    }
+
+    const update = {
       name: req.body.name,
-      lat: req.body.lat,
-      lon: req.body.lon,
+      coords: req.body.coords,
+      slug: req.body.slug,
+      short_description: req.body.short_description,
+      website: req.body.website,
+    };
+
+    if (req.file) {
+      const pathWithoutPublic = req.file.path.split('/').slice(1).join('/');
+      update["featured_image"] = pathWithoutPublic;
+    }
+
+    try {
+      const saved = await Camping.findOneAndUpdate(filter, update, {
+        upsert: true,
+        useFindAndModify: false,
+        new: true,
+      });
+
       //close_villages: res.locals.closestVillages.map((village) => village._id),
       //geo_units: [...geoUnits],
-      closest_village: {
+      /* closest_village: {
         name: res.locals.closestVillages[0].name,
         distance: res.locals.closestVillages[0].distance,
-      },
-    });
+      }, 
+    }); */
 
-    const saved = await camp.save();
-
-    res.json(saved);
+      res.json({status: "saved"});
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
   },
 ];
 
@@ -118,9 +166,24 @@ exports.camping_close_get = async function (req, res, next) {
 
 exports.camping_get_list = async function (req, res, next) {
   try {
-    const response = await Camping.find().lean()
-    res.json(response)
+    const response = await Camping.find().lean();
+    res.json(response);
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
+
+exports.camping_slug_exists_get = async function (req, res, next) {
+  const slug = req.query.slug;
+
+  try {
+    const response = await Camping.find({ slug: slug }).lean();
+    if (response.length === 0) {
+      res.json({ unique: true });
+    } else {
+      res.json({ unique: false });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
