a3e6f16eb37a6740ee8ef0d23a573cab328aa52a
Peter Smid <petosmid@gmail.com>
2021-10-31 13:54:50 +0100

    fix validation result handler middleware

--- middleware/validationResultHanderMiddleware.js
+++ middleware/validationResultHanderMiddleware.js
@@ -3,9 +3,7 @@ const { validationResult } = require("express-validator");
 module.exports = function validationResultsHandler(req, res, next) {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    res.status(400)
-      .json({ ...errors });
-    return;
+    return res.status(400).json({ errors: errors.array() });
   } else {
     next();
   }
