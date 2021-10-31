103e9c99ea67b23299a9e27090371c73d0029270
Peter Smid <petosmid@gmail.com>
2021-10-31 13:55:49 +0100

    add route to check slug uniqueness

--- routes/api/campingRoutes.js
+++ routes/api/campingRoutes.js
@@ -6,6 +6,8 @@ router.get('/',  campingController.camping_get);
 router.post('/', campingController.camping_create_post)
 router.get('/list', campingController.camping_get_list)
 router.get('/close/', campingController.camping_close_get);
+router.get('/slug-check', campingController.camping_slug_exists_get);
 router.get('/:id',  campingController.camping_detail_get);
 
 module.exports = router;
+
