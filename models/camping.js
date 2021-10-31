fc5e8559fde00d41770994994143eebae95915e3
Peter Smid <petosmid@gmail.com>
2021-10-31 13:54:58 +0100

    update model representation

--- models/camping.js
+++ models/camping.js
@@ -1,22 +1,17 @@
-var mongoose = require("mongoose");
-var Schema = mongoose.Schema;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-var CampingSchema = new Schema(
+const CampingSchema = new Schema(
   {
     name: { type: String, required: true },
-    tagline: { type: String, required: false, minLength: 10, maxLength: 60 },
-    description: { type: String, required: false, minLength: 20 },
-    lat: { type: Number, required: true },
-    lon: { type: Number, required: true },
-    close_villages: [{ type: Schema.Types.ObjectId, ref: "Village" }],
-    geo_units: [{ type: Schema.Types.ObjectId, ref: "GeoUnit" }],
-    closest_village: {
-      name: String,
-      distance: Number,
-    },
+    slug: { type: String, required: true },
+    short_description: { type: String, required: false, minLength: 20 },
+    website: { type: String, required: false },
+    featured_image: { type: String, required: false },
+    coords: { lat: { type: Number, required: true }, lon: { type: Number, required: true } },
   },
   {
-    strict:false,
+    strict: false,
     collection: "campings",
   }
 );
