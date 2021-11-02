7b3f58271ac81bb5d8c016403cacd1be413554fa
Peter Smid <petosmid@gmail.com>
2021-11-02 12:08:29 +0100

    add new route for camp search

--- routes/api/searchRoutes.js
+++ routes/api/searchRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const searchController = require('../../controllers/searchController')
 
 router.get('/', searchController.search_get);
-
+router.get('/camps', searchController.fulltext_camp_search)
 
 module.exports = router;
