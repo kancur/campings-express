641d49e056e3b7f2a72fbf6da76d79ec89e19f4c
Peter Smid <petosmid@gmail.com>
2021-11-08 22:37:06 +0100

    remove global endpoint and lint

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,24 +1,22 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-const geoUnitRoutes = require("./geoUnitRoutes");
-const campingRoutes = require("./campingRoutes");
-const villageRoutes = require("./villageRoutes");
-const combinedRoutes = require("./combinedRoutes");
-const searchRoutes = require("./searchRoutes");
-const adminRoutes = require("./adminRoutes");
-const waterbodyRoutes = require("./waterbodyRoutes");
+const geoUnitRoutes = require('./geoUnitRoutes');
+const campingRoutes = require('./campingRoutes');
+const villageRoutes = require('./villageRoutes');
+const combinedRoutes = require('./combinedRoutes');
+const searchRoutes = require('./searchRoutes');
+const adminRoutes = require('./adminRoutes');
+const waterbodyRoutes = require('./waterbodyRoutes');
+const authRoutes = require('./authRoutes');
 
-router.use("/geo/", geoUnitRoutes);
-router.use("/camping/", campingRoutes);
-router.use("/village/", villageRoutes);
-router.use("/combined/", combinedRoutes);
-router.use("/search/", searchRoutes);
-router.use("/admin/", adminRoutes);
-router.use("/waterbody/", waterbodyRoutes);
-
-router.get("/", function (req, res, next) {
-  res.json({ title: "V1 API" });
-});
+router.use('/geo', geoUnitRoutes);
+router.use('/camping', campingRoutes);
+router.use('/village', villageRoutes);
+router.use('/combined', combinedRoutes);
+router.use('/search', searchRoutes);
+router.use('/admin', adminRoutes);
+router.use('/waterbody', waterbodyRoutes);
+router.use('/auth', authRoutes);
 
 module.exports = router;
