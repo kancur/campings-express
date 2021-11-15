d24f643d184fb76a4d37d89337c838d72f69c18d
Peter Smid <petosmid@gmail.com>
2021-11-15 21:53:42 +0100

    fix stalled request in error handler

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -9,6 +9,7 @@ const requireAuth = (req, res, next) => {
       if (err) {
         res.locals.user = null;
         console.log(err.message)
+        next()
       } else {
         const user = await User.findById(decodedToken.id).select('-password -__v').lean()
         res.locals.user = user
