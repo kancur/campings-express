52932e989e270b55a089834f8c7a26f7745a4d7a
Peter Smid <petosmid@gmail.com>
2021-11-16 16:55:14 +0100

    add logging

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,6 +4,10 @@ const User = require('../models/User');
 const requireAuth = (req, res, next) => {
   
   const token = req.cookies.jwt
+
+  console.log('--requiring auth--')
+  console.log('--jwt token ---> ', token)
+
   if (token) {
     jwt.verify(token, process.env.SECRET, async (err, decodedToken) => {
       if (err) {
@@ -12,6 +16,9 @@ const requireAuth = (req, res, next) => {
         next()
       } else {
         const user = await User.findById(decodedToken.id).select('-password -__v').lean()
+  
+        console.log('--user ---> ', user)
+
         res.locals.user = user
         next()
       }
