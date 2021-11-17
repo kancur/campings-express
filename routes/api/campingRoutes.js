a012f2dbeab89c6c6a243886bc52934dca7dcf90
Peter Smid <petosmid@gmail.com>
2021-11-17 10:30:04 +0100

    change slug route

--- routes/api/campingRoutes.js
+++ routes/api/campingRoutes.js
@@ -7,7 +7,7 @@ router.post('/', campingController.camping_create_post)
 router.get('/list', campingController.camping_get_list)
 router.get('/close/', campingController.camping_close_get);
 router.get('/slug-check', campingController.camping_slug_exists_get);
-router.get('/:slug',  campingController.camping_slug_get);
+router.get('/slug/:slug',  campingController.camping_slug_get);
 
 module.exports = router;
 
