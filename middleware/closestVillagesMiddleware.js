d170463fb42ce90a3b7a8302c6d82968f135a294
Peter Smid <petosmid@gmail.com>
2021-12-13 15:55:33 +0100

    limit results to 5

--- middleware/closestVillagesMiddleware.js
+++ middleware/closestVillagesMiddleware.js
@@ -2,7 +2,7 @@ const getClosestVillages = require('../helpers/getClosestVillages');
 
 module.exports = async function closestVillageMiddleware(req, res, next) {
   //const { lat, lon } = req.body.coords;
-  const closestVillages = await getClosestVillages(req.body.coords);
+  const closestVillages = await getClosestVillages(req.body.coords, 5);
   res.locals.closestVillages = closestVillages;
 
   next();
