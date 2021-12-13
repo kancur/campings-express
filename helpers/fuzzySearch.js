6fb93ce6cb3f5ec257f6a103eb1af7f484bb79bc
Peter Smid <petosmid@gmail.com>
2021-12-13 16:42:01 +0100

    add automatic updating

--- helpers/fuzzySearch.js
+++ helpers/fuzzySearch.js
@@ -1,10 +1,11 @@
-const Fuse = require('./fuse.common.js')
-const { getMergedSearchData } = require('../helpers/getMergedSearchData')
-const diacritics = require('diacritics')
+const Fuse = require('./fuse.common.js');
+const { getMergedSearchData } = require('../helpers/getMergedSearchData');
+const diacritics = require('diacritics');
+const FUSE_UPDATE_INTERVAL = 1000 * 30;
 
 function getFn() {
-  const value =  Fuse.config.getFn.apply(this, arguments)
-  const cleaned = diacritics.remove(value.toLowerCase())
+  const value = Fuse.config.getFn.apply(this, arguments);
+  const cleaned = diacritics.remove(value.toLowerCase());
   return cleaned;
 }
 
@@ -12,25 +13,30 @@ const options = {
   includeScore: false,
   includeMatches: false,
   includeRefIndex: false,
-   findAllMatches: true,
+  findAllMatches: true,
   // minMatchCharLength: 1,
   threshold: 0.25,
   // ignoreFieldNorm: false,
   getFn: getFn,
-  keys: [
-    "name",
-  ]
+  keys: ['name'],
 };
 
-const fuse = new Fuse([], options)
+const fuse = new Fuse([], options);
 
-
-getMergedSearchData()
-  .then((data) => fuse.setCollection(data))
+let timestamp;
+function updateFuse() {
+  getMergedSearchData()
+    .then((data) => fuse.setCollection(data))
+    .then(() => timestamp = Date.now());
+}
+updateFuse();
 
 module.exports = async function fuzzySearch(query) {
-  const resultArray = await fuse.search(diacritics.remove(query))
-  const sliced = resultArray.slice(0,10)
-  const cleaned = sliced.map(({item, score}) => ({...item, score}))
-  return cleaned
-}
+  if (Date.now() - timestamp > FUSE_UPDATE_INTERVAL) {
+    updateFuse();
+  }
+  const resultArray = await fuse.search(diacritics.remove(query));
+  const sliced = resultArray.slice(0, 10);
+  const cleaned = sliced.map(({ item, score }) => ({ ...item, score }));
+  return cleaned;
+};
